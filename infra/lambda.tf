52e1201857896b61c33e03f726f55e00a8a89aaa
sairam619 <sairamgangapuram2001@gmail.com>
2026-05-25 19:02:51 +0530

    Changes made in such a way that now Terraform will automatically zip the Lambda code!

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,14 +1,31 @@
+#################################################
+# PACKAGE LAMBDA
+#################################################
+
+data "archive_file" "lambda_zip" {
+
+  type = "zip"
+
+  source_dir  = "../src/lambda_code"
+
+  output_path = "../src/lambda_code/lambda.zip"
+}
+
+#################################################
+# LAMBDA FUNCTION
+#################################################
+
 resource "aws_lambda_function" "lambda_function" {
 
   function_name = var.lambda_function_name
 
-  role = aws_iam_role.lambda_role.arn
+  role = var.lambda_role_arn
 
   handler = "app.lambda_handler"
 
   runtime = "python3.14"
 
-  filename = "../src/lambda_code/lambda.zip"
+  filename = data.archive_file.lambda_zip.output_path
 
-  source_code_hash = filebase64sha256("../src/lambda_code/lambda.zip")
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 }
